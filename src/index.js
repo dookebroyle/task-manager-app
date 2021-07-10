b7ac7f18cca630c22212c3a863cdbba6cddcd28a
dookebroyle <57194791+dookebroyle@users.noreply.github.com>
2021-07-10 09:18:14 -0600

    add routes for updating existing tasks and users

--- src/index.js
+++ src/index.js
@@ -8,63 +8,124 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/users', (req, res) => {
-    const user = new User(req.body).save().then(() => {
-        res.status(210).send(user);
-    }).catch( e=> {
+//create new user
+app.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try{
+        await user.save()
+        res.status(201).send(user)
+    } catch (e)  {
         res.status(400).send(e)
-    })
+    }
+
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then( users => {
+//get all users
+app.get('/users', async (req, res) => {
+    try{
+        const users = await User.find({})
         res.send(users)
-    }).catch( e => {    
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
-app.get('/users/:id', (req, res) => {
-    console.log(req.params.id)
-    const _id = req.params.id
-    User.findById(_id).then( user => {
-        console.log(user)
+//get one user
+app.get('/users/:id', async (req, res) => {
+    const id = req.params.id
+    try{
+        const user = await User.findById(id)
         if(!user){
             return res.status(404).send()
         }
         res.send(user)
-    }).catch( e => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
-app.post('/tasks', (req, res) =>{
-    const task = new Task(req.body).save().then(() => {
+//update existing user
+app.patch('/users/:id', async (req, res) => {
+    const id = req.params.id
+    const allowedUpdates = [ 'name', 'email', 'password', 'age']
+    const updates = Object.keys(req.body)
+    console.log(updates)
+    const isValidOperation = updates.every( update => allowedUpdates.includes(update))
+    
+    console.log(isValidOperation)
+    if (!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+    
+    try{
+        const user = await User.findByIdAndUpdate(id, req.body, {new: true, runValidators: true})
+        if (!user){
+            res.status(404).send()
+        }
+        res.send(user)
+    }catch (e) {
+        res.status(500).send()
+    }
+})
+
+
+//create new task
+app.post('/tasks', async (req, res) =>{
+    const task = new Task(req.body)
+    try{
+        await task.save()
         res.status(201).send(task)
-    }).catch( e => {
-        res.status(400).send(e)
-    })
-})
-
-
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch( e => {
+    } catch (e) {
         res.status(400).send()
-    })
+    }
+
 })
 
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id
-    Task.findById(_id).then(task => {
+//get all tasks
+app.get('/tasks', async (req, res) => {
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//get one task
+app.get('/tasks/:id', async (req, res) => {
+    const id = req.params.id
+    try{
+        const task = await Task.findById(id)
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//update existing task
+app.patch('/tasks/:id', async (req, res) => {
+    const id = req.params.id
+    const allowedUpdates = [ 'description','completed']
+    const updates = Object.keys(req.body)
+    const isValidOperation = updates.every( update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+
+
+    try{
+        const task = await Task.findByIdAndUpdate(id, req.body, {new: true, runValidators: true})
         if(!task) {
             res.status(404).send()
         }
         res.send(task)
-    }).catch( e => {
-        res.status(500).send(e)
-    })
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 
 
