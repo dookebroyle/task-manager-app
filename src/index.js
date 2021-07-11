7b0c50d66ac4b3e1a2e8e9cc8db0bb54fad07cc7
dookebroyle <57194791+dookebroyle@users.noreply.github.com>
2021-07-11 15:10:55 -0600

    added file cropping and formatting

--- src/index.js
+++ src/index.js
@@ -6,14 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images'
-})
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-})
-
 app.use(express.json()) //recognize incoming request object as json
 //see routers folder for routes
 app.use(userRouter)
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
-
-
-
 
 //create new user
 router.post('/users', async (req, res) => {
@@ -60,8 +58,6 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     }
 })
 
-
-
 //update existing user
 router.patch('/users/me', auth, async (req, res) => {
     const allowedUpdates = [ 'name', 'email', 'password', 'age']
@@ -75,9 +71,6 @@ router.patch('/users/me', auth, async (req, res) => {
         await req.user.save()
         res.send(req.user)
     }catch (e) {
-    //    if(e.errors.password.properties.message){
-    //     return res.status(400).send(e.errors.password.properties.message)
-    //    }
         res.status(500).send(e)
     }
 })
@@ -92,17 +85,49 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
-
-
+//upload avatar to profile
 const upload = multer({
-    dest: 'avatars'
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        //restrict file type
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload an image'))
+        }
+        return cb(undefined, true)
+    }
 })
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+
+//upload an avatar image
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send ({error: error.message })
+})
+
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
     res.send()
 })
 
-
-
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar) {
+            throw new Error('No image found')
+        }
+        res.set('Content-Type','image/png').send(user.avatar)
+    } catch (e) {
+        res.status(404).send(e)
+    }
+})
 
 
 
